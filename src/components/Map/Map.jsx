d505f9f4f05442b0142189fe66e388334441af7a
a-bayborosha <anton.bayborosha@gmail.com>
2020-04-01 11:47:28 +0200

    adding a map image

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import classes from "./Map.module.css";
+import mapImg from "./../../img/map.jpg";
 
 const Map = () => {
-  return <div className={classes.Map}></div>;
+  return (
+    <div className={classes.Map}>
+      <img src={mapImg} alt="" className={classes.mapImg} />
+    </div>
+  );
 };
 
 export default Map;
